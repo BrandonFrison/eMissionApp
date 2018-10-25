f4ba406f38ce57905352b20d8d431a0d5f52eee9
John <jjankows@sfu.ca>
2018-10-24 17:33:21 -0700

    Changed icons and names of nav drawer menu objects.
    New left & right arrow icons for survey.

--- app/src/main/java/ca/cmpt276/greengoblins/emission/SurveyActivity.java
+++ app/src/main/java/ca/cmpt276/greengoblins/emission/SurveyActivity.java
@@ -208,17 +208,15 @@ public class SurveyActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_calculator) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_pledge) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_history) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_community) {
 
         }
 
