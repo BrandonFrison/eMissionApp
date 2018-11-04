da2941cd39fbbc8a64db0e5e0e34c391f6a2c165
Harman <hba23@sfu.ca>
2018-11-04 13:39:51 -0800

    Created button on PledgeStatistics page that leads to the MakeYourOwnPledgePage

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/subfragments/PledgeStatistics.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/subfragments/PledgeStatistics.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.text.SpannableString;
 import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import ca.cmpt276.greengoblins.emission.R;
+import ca.cmpt276.greengoblins.fragments.HistoryFragment;
 
 public class PledgeStatistics extends Fragment {
     View view = null;
     TextView TotalCo2eAmount_Pledge;
     TextView ConvectionText;
+    Button mMakeYourOwnPledge;
+
     int convectionNum_tree = 63;
     int convectionNum_forest = 2;
     int convectionNum_driving = 2222;
@@ -43,7 +49,19 @@ public class PledgeStatistics extends Fragment {
         TotalCo2eAmount_Pledge.setText(ss1);
 
 
+        mMakeYourOwnPledge = view.findViewById(R.id.pledge_statistics_make_your_own_pledge);
+        mMakeYourOwnPledge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment makeYourOwnPledge = new MakeYourOwnPledge();
 
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace( R.id.frame_activity_content, makeYourOwnPledge );
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
         //convection
         ConvectionText = view.findViewById(R.id.convection_text);
