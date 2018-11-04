467864efbea88b6d3a8147a55b766249c7ad91de
Harman <hba23@sfu.ca>
2018-11-04 12:11:46 -0800

    Renamed files

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/subfragments/PledgeStatistics.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/subfragments/PledgeStatistics.java
@@ -1,23 +1,19 @@
 package ca.cmpt276.greengoblins.fragments.subfragments;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import ca.cmpt276.greengoblins.emission.R;
 
-public class AllCo2ePledgeAmount extends Fragment {
+public class PledgeStatistics extends Fragment {
     View view = null;
     TextView TotalCo2eAmount_Pledge;
     TextView ConvectionText;
@@ -32,7 +28,7 @@ public class AllCo2ePledgeAmount extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.allco2eamount,container,false);
+        View view = inflater.inflate(R.layout.pledge_statistics,container,false);
 
 
         //total co2e
