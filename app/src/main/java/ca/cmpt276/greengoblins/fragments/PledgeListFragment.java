3146390d90fa7216dd8402862f5eb81798bf8dcf
John <jjankows@sfu.ca>
2018-11-04 18:15:23 -0800

    Finish layout of pledge list page including search box, filter dropdown, and RecyclerView for database list.

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/PledgeListFragment.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/PledgeListFragment.java
@@ -7,32 +7,46 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import ca.cmpt276.greengoblins.emission.MainActivity;
 import ca.cmpt276.greengoblins.emission.R;
 
 public class PledgeListFragment extends Fragment {
-   /* View view = null;
-    TextView CurrentPledge;
-    TextView PledgeList;
-    boolean MyPledgeExist = false;*/
+
     MainActivity mMainActivity;
+    private TextView mPledgeListView;
     private Button mMakePledgeButton;
+    private Spinner mFilterDropdown;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_pledge_list, container,false);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         mMainActivity = (MainActivity) getActivity();
 
+        mPledgeListView = (TextView) view.findViewById(R.id.pledge_list_text);
+        mPledgeListView.setText(R.string.pledge_list_text);
+
         mMakePledgeButton = (Button) view.findViewById(R.id.button_make_pledge);
+
+        mFilterDropdown = (Spinner) view.findViewById(R.id.dropdown_filter);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mMainActivity,
+                R.array.pledge_list_filters, android.R.layout.simple_spinner_item);
+
+        //adapter.setDropDownViewResource(R.layout.);
+
+        mFilterDropdown.setAdapter(adapter);
+
 
         mMakePledgeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,22 +55,5 @@ public class PledgeListFragment extends Fragment {
                 mMainActivity.startFragment( newFragment, true, false);
             }
         });
-        /*CurrentPledge = view.findViewById(R.id.mycurrentpledge);
-        String CurrentPledge_text;
-        if(MyPledgeExist == false){ //if users haven't made a pledge yet
-            CurrentPledge_text = "Your Pledge: \n\tYou haven't made a pledge yet!";
-        }
-        else{
-            CurrentPledge_text = "";
-        }
-
-        CurrentPledge.setText(CurrentPledge_text);
-
-
-
-        PledgeList = view.findViewById(R.id.pledgesList);
-
-
-        return  view;*/
     }
 }
