3c74ded207f25e7def974c99d18c3607b9ba93fd
John <jjankows@sfu.ca>
2018-11-04 20:03:26 -0800

    Finish layout of create pledge page. Remove vestigial subfragment classes and layouts.
    -John Jankowski

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/MakePledgeFragment.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/MakePledgeFragment.java
@@ -7,47 +7,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
+import ca.cmpt276.greengoblins.emission.MainActivity;
 import ca.cmpt276.greengoblins.emission.R;
 
 public class MakePledgeFragment extends Fragment {
-    /*View view = null;
-    TextView userInformation;
-    TextView pledgeamount;
-    CheckBox ShowName;
-    Button SharePledgeButton;
-    Button PublishPleDgeButton;
-    String FirstName = "John";//should let user input this value
-    String LastName = "Smith";//should let user input this value
-    int ValueSavedFromMealPlan = 88;//should let user input this value
-    int CostomValue = 0;//should let user input this value*/
+    MainActivity mMainActivity;
+    private Button mSharePledgeButton;
+    private Button mPublishPledgeButton;
 
+    private TextView mFirstName;
+    private TextView mLastName;
+    private TextView mMunicipality;
+    private TextView mPledgeAmount;
+    private CheckBox mShowNameCheckbox;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_pledge_create, container,false);
-       /*userInformation = view.findViewById(R.id.UserInformation);
-        String userInformation_text = "First Name: \t"+FirstName+
-                "Last Name: \t"+LastName;
-        userInformation.setText(userInformation_text);
-
-
-        pledgeamount = view.findViewById(R.id.PledgeAmount);
-        String pledgeamount_text ="Pledge Amount:" +
-                "   Saving from Meal Plan:\t"+ValueSavedFromMealPlan+"\tTonnes CO2e"+
-                "Custom: \t\t\t"+CostomValue+"\tTonnes CO2e";
-        pledgeamount.setText(pledgeamount_text);
-
-        ShowName = view.findViewById(R.id.checkBox);
-        SharePledgeButton = view.findViewById(R.id.sharepledgebutton);
-        PublishPleDgeButton = view.findViewById(R.id.publishpledgebutton);
-
-        return view;*/
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mMainActivity = (MainActivity) getActivity();
+        mSharePledgeButton = (Button) view.findViewById(R.id.button_share_pledge);
+        mPublishPledgeButton = (Button) view.findViewById(R.id.button_publish_pledge);
 
+        mFirstName = (TextView) view.findViewById(R.id.input_first_name);
+        mLastName = (TextView) view.findViewById(R.id.input_last_name);
+        mMunicipality = (TextView) view.findViewById(R.id.input_municipality);
+        mPledgeAmount = (TextView) view.findViewById(R.id.input_pledge_amount);
+        mShowNameCheckbox = (CheckBox) view.findViewById(R.id.checkbox_show_name);
+
+        mSharePledgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Fragment newFragment = new MakePledgeFragment();
+                //mMainActivity.startFragment( newFragment, true, false);
+            }
+        });
+        mPublishPledgeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Fragment newFragment = new PledgeListFragment();
+                //mMainActivity.startFragment( newFragment, true, false);
+            }
+        });
     }
 }
