1a2289a2080a5d5803e1e5d509fe0de8da90c9ea
Harman <hba23@sfu.ca>
2018-11-06 02:11:03 -0800

    Add the ability to create a pledge associated with a user's account and have to recorded in the database.

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/MakePledgeFragment.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/MakePledgeFragment.java
@@ -1,16 +1,25 @@
 package ca.cmpt276.greengoblins.fragments;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import ca.cmpt276.greengoblins.emission.MainActivity;
 import ca.cmpt276.greengoblins.emission.R;
@@ -21,10 +30,10 @@ public class MakePledgeFragment extends Fragment {
     private Button mSharePledgeButton;
     private Button mPublishPledgeButton;
 
-    private TextView mFirstName;
-    private TextView mLastName;
-    private TextView mMunicipality;
-    private TextView mPledgeAmount;
+    EditText mFirstNameInputField;
+    EditText mLastNameInputField;
+    EditText mMunicipalityInputField;
+    EditText mPledgeAmountInputField;
     private CheckBox mShowNameCheckbox;
 
     @Nullable
@@ -39,66 +48,80 @@ public class MakePledgeFragment extends Fragment {
         mSharePledgeButton = (Button) view.findViewById(R.id.button_share_pledge);
         mPublishPledgeButton = (Button) view.findViewById(R.id.button_publish_pledge);
 
-        mFirstName = (TextView) view.findViewById(R.id.input_first_name);
-        mFirstName.setText(mMainActivity.getCurrentUser().getEmail());
-        mLastName = (TextView) view.findViewById(R.id.input_last_name);
-        mMunicipality = (TextView) view.findViewById(R.id.input_municipality);
-        mPledgeAmount = (TextView) view.findViewById(R.id.input_pledge_amount);
+        mFirstNameInputField = (EditText) view.findViewById(R.id.input_first_name);
+        //mFirstNameInputField.setText(mMainActivity.getCurrentUser().getU());
+        mLastNameInputField = (EditText) view.findViewById(R.id.input_last_name);
+        mMunicipalityInputField = (EditText) view.findViewById(R.id.input_municipality);
+        mPledgeAmountInputField = (EditText) view.findViewById(R.id.input_pledge_amount);
         mShowNameCheckbox = (CheckBox) view.findViewById(R.id.checkbox_show_name);
 
 
         mSharePledgeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if( !mMainActivity.checkUserLogin() ){
-                    Toast.makeText( mMainActivity, R.string.error_user_not_logged_in, Toast.LENGTH_LONG ).show();
-                } else {
+                if( !mMainActivity.checkUserLogin() ) {
+                    Toast.makeText(mMainActivity, R.string.error_user_not_logged_in, Toast.LENGTH_LONG).show();
+                    mMainActivity.popupLogin();
+                } else { //  TO DO: Add another else if condition to check if the account has a valid pledge to share
+
+                    // Just commented out temporarily since it could potentially lead to bugs
+                    /*
                     User newUser = createUserFromForm( mMainActivity.getCurrentUser().getEmail() );
                     if( newUser != null ) {
                         mMainActivity.updateUserData(newUser);
                         Toast.makeText(mMainActivity, "success", Toast.LENGTH_SHORT).show();
                     }
+                    */
+                    Toast.makeText(mMainActivity, "TO DO: Implement a share pop up", Toast.LENGTH_SHORT).show();
                 }
-
-
-
-                //Fragment newFragment = new MakePledgeFragment();
-                //mMainActivity.startFragment( newFragment, true, false);
             }
         });
         mPublishPledgeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Fragment newFragment = new PledgeListFragment();
-                //mMainActivity.startFragment( newFragment, true, false);
+                if( !mMainActivity.checkUserLogin() ) {
+                    Toast.makeText(mMainActivity, R.string.error_user_not_logged_in, Toast.LENGTH_LONG).show();
+                    mMainActivity.popupLogin();
+                }
+                else {
+                    String firstName = "";
+                    String lastName = "";
+                    String city = "";
+                    String email = "";
+                    Double pledgeAmount = 0.0;
+
+                    firstName = mFirstNameInputField.getText().toString().trim();
+                    lastName = mLastNameInputField.getText().toString().trim();
+                    city = mMunicipalityInputField.getText().toString().trim();
+                    email = mMainActivity.getCurrentUser().getEmail();
+                    String pledgeAmountString = mPledgeAmountInputField.getText().toString().trim();
+
+                    if(isInputValid(firstName, lastName, pledgeAmountString)) {
+                        pledgeAmount = Double.parseDouble(pledgeAmountString);
+
+                        DatabaseReference usersDatabase;
+                        usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+                        String userID = mMainActivity.getCurrentUser().getUid();
+                        User user = new User(email, firstName, lastName, city, pledgeAmount);
+                        usersDatabase.child(userID).setValue(user);
+                    }
+                }
             }
         });
     }
 
     private User createUserFromForm(String userEmail){
-        String firstName = mFirstName.getText().toString();
-        String lastName = mLastName.getText().toString();
-        String municipality = mMunicipality.getText().toString();
-        String pledgeAmount = mPledgeAmount.getText().toString();
+        String firstName = mFirstNameInputField.getText().toString();
+        String lastName = mLastNameInputField.getText().toString();
+        String municipality = mMunicipalityInputField.getText().toString();
+        String pledgeAmount = mPledgeAmountInputField.getText().toString();
         User userData = null;
 
-        if( firstName.isEmpty() ){
-            Toast.makeText( mMainActivity, R.string.empty_firstname_message, Toast.LENGTH_SHORT ).show();
+        if( !isInputValid(firstName, lastName, pledgeAmount) ) {
             return userData;
-        }else if( lastName.isEmpty() ){
-            Toast.makeText( mMainActivity, R.string.empty_lastname_message, Toast.LENGTH_SHORT ).show();
-            return  userData;
-        }else if( pledgeAmount.isEmpty() ){
-            Toast.makeText( mMainActivity, R.string.empty_pledge_message, Toast.LENGTH_SHORT ).show();
-            return  userData;
-        }else{
-            double pledgeValue;
-            try{
-                pledgeValue = Double.parseDouble(pledgeAmount);
-            } catch (Exception e){
-                pledgeValue = 0;
-                Toast.makeText( mMainActivity, R.string.error_bad_pledge_format, Toast.LENGTH_LONG ).show();
-            }
+        }else {
+            double pledgeValue = Double.parseDouble(pledgeAmount);
             userData = new User(
                     userEmail,
                     firstName,
@@ -112,5 +135,38 @@ public class MakePledgeFragment extends Fragment {
             }
         }
         return userData;
+    }
+
+    private boolean isInputValid(String firstName, String lastName, String pledgeAmount) {
+        boolean isValid = true;
+
+        if( firstName.isEmpty() ){
+            Toast.makeText( mMainActivity, R.string.empty_firstname_message, Toast.LENGTH_SHORT ).show();
+            isValid = false;
+        }
+        else if( lastName.isEmpty() ){
+            Toast.makeText( mMainActivity, R.string.empty_lastname_message, Toast.LENGTH_SHORT ).show();
+            isValid = false;
+        }
+        else if( pledgeAmount.isEmpty() ) {
+            Toast.makeText(mMainActivity, R.string.empty_pledge_message, Toast.LENGTH_SHORT).show();
+            isValid = false;
+        }
+        else {
+            double pledgeValue = 0;
+            try{
+                pledgeValue = Double.parseDouble(pledgeAmount);
+            } catch (Exception e) {
+                Toast.makeText( mMainActivity, R.string.error_bad_pledge_format, Toast.LENGTH_LONG ).show();
+                isValid = false;
+            }
+
+            if(pledgeValue < 0) {
+                Toast.makeText( mMainActivity, R.string.error_bad_pledge_format, Toast.LENGTH_LONG ).show();
+                isValid = false;
+            }
+        }
+
+        return isValid;
     }
 }
