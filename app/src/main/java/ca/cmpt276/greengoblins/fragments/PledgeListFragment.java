4d57f4f4534e4019e9a62bb85cfd1ecd67fb60ec
Harman <hba23@sfu.ca>
2018-11-07 21:34:34 -0800

    Display pledges made by other users on pledge list page.

--- app/src/main/java/ca/cmpt276/greengoblins/fragments/PledgeListFragment.java
+++ app/src/main/java/ca/cmpt276/greengoblins/fragments/PledgeListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +15,20 @@ import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import ca.cmpt276.greengoblins.emission.MainActivity;
 import ca.cmpt276.greengoblins.emission.R;
+import ca.cmpt276.greengoblins.foodsurveydata.Convertor;
+import ca.cmpt276.greengoblins.foodsurveydata.User;
+import ca.cmpt276.greengoblins.foodsurveydata.UserAdapter;
 
 public class PledgeListFragment extends Fragment {
 
@@ -22,6 +36,10 @@ public class PledgeListFragment extends Fragment {
     private TextView mPledgeListView;
     private Button mMakePledgeButton;
     private Spinner mFilterDropdown;
+
+    private RecyclerView mRecyclerView;
+    private UserAdapter mAdapter;
+    private List<User> mUserList;
 
     @Nullable
     @Override
@@ -35,10 +53,17 @@ public class PledgeListFragment extends Fragment {
 
         mPledgeListView = (TextView) view.findViewById(R.id.pledge_list_text);
         mPledgeListView.setText(R.string.pledge_list_text);
-
         mMakePledgeButton = (Button) view.findViewById(R.id.button_make_pledge);
-
         mFilterDropdown = (Spinner) view.findViewById(R.id.dropdown_filter);
+
+        mUserList = new ArrayList<User>();
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.pledge_list_view) ;
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mMainActivity.getBaseContext()));
+        mAdapter = new UserAdapter(mMainActivity.getBaseContext(), mUserList);
+        mRecyclerView.setAdapter(mAdapter);
+        displayData();
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mMainActivity,
                 R.array.pledge_list_filters, android.R.layout.simple_spinner_item);
@@ -47,12 +72,32 @@ public class PledgeListFragment extends Fragment {
 
         mFilterDropdown.setAdapter(adapter);
 
-
         mMakePledgeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Fragment newFragment = new MakePledgeFragment();
                 mMainActivity.startFragment( newFragment, true, false);
+            }
+        });
+    }
+
+    public void displayData() {
+        final DatabaseReference usersDatabase;
+        usersDatabase = FirebaseDatabase.getInstance().getReference("Users");
+
+        usersDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    User userInfo = (User) snapshot.getValue(User.class);
+                    mUserList.add(userInfo);
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
     }
