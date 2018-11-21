b7621b0f3f852f3cdb5998d4a500d26d2d1019ff
John <jjankows@sfu.ca>
2018-11-20 21:08:56 -0800

    change history to save to "foodsuveydata" directory instead of project directory
    history sorted when loading so no longer added to linegraph out of order

--- app/src/main/java/ca/cmpt276/greengoblins/foodsurveydata/FoodSurveyHistoryManager.java
+++ app/src/main/java/ca/cmpt276/greengoblins/foodsurveydata/FoodSurveyHistoryManager.java
@@ -15,10 +15,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.regex.Pattern;
 
 public class FoodSurveyHistoryManager {
 
+    private File saveFolder;
     private final String[] TAGS = {"SAVE_EXCEPTION", "LOAD_EXCEPTION"};
     private ArrayList<String> tableDates;
     private ArrayList<ConsumptionTable> tableHistory;
@@ -26,6 +29,22 @@ public class FoodSurveyHistoryManager {
     public FoodSurveyHistoryManager() {
         tableHistory = new ArrayList<ConsumptionTable>();
         tableDates = new ArrayList<String>();
+    }
+
+    public boolean createSaveFolder(Context activityContext){
+        if( saveFolder != null ) return true;
+
+        File path = new File(activityContext.getFilesDir(), "foodsurveydata");
+        if ( !path.exists() ) {
+            path.mkdirs();
+        }
+
+        saveFolder = path;
+
+        if ( saveFolder == null ) {
+            return false;
+        }
+        return true;
     }
 
     public ArrayList<ConsumptionTable> getTableHistory() {
@@ -41,13 +60,14 @@ public class FoodSurveyHistoryManager {
     }
 
     public boolean saveTableByDate(Context activityContext, ConsumptionTable tableToSave) {
+        createSaveFolder(activityContext);
+
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat mdformat = new SimpleDateFormat("yy-MM-dd");
-        String fileName = mdformat.format(calendar.getTime());
-        //DateFormat df = DateFormat.getDateInstance();
-        //String fileName = df.format(new Date());
+        SimpleDateFormat mdFormat = new SimpleDateFormat("yy-MM-dd");
+        String fileName = mdFormat.format(calendar.getTime());
         try {
-            FileOutputStream fos = activityContext.openFileOutput(fileName, Context.MODE_PRIVATE);
+            File newSave = new File(saveFolder, fileName);
+            FileOutputStream fos = new FileOutputStream(newSave);
             ObjectOutputStream os = new ObjectOutputStream(fos);
             os.writeObject(tableToSave);
             os.close();
@@ -64,29 +84,32 @@ public class FoodSurveyHistoryManager {
     }
 
     public void deleteTableByDate(Context activityContext, String formattedDate){
-        String[] fileList = activityContext.fileList();
-        for (String fileName : fileList) {
-            Log.d(TAGS[1], "file: " + fileName);
-            if (fileName.equals(formattedDate)) {
+        createSaveFolder(activityContext);
 
+        File[] fileList = saveFolder.listFiles();
+        for( File listedFile : fileList ){
+            if( listedFile.getName().equals(formattedDate) ){
                 tableHistory.remove(tableDates.indexOf(formattedDate));
                 tableDates.remove(formattedDate);
-                File file = new File(activityContext.getFilesDir(), fileName);
-                file.delete();
+                listedFile.delete();
             }
         }
     }
 
     public ConsumptionTable loadTableByDate(Context activityContext, String formattedDate) {
+        createSaveFolder(activityContext);
+
         ConsumptionTable loaded = null;
         try {
-            FileInputStream fis = activityContext.openFileInput(formattedDate);
+            File newLoad = new File( saveFolder, formattedDate );
+            FileInputStream fis = new FileInputStream(newLoad);
             ObjectInputStream is = new ObjectInputStream(fis);
             loaded = (ConsumptionTable) is.readObject();
             is.close();
             fis.close();
             Log.d(TAGS[1], "loaded: " + formattedDate );
         } catch (FileNotFoundException exception) {
+            Log.d(TAGS[1], "junkle time");
             Log.d(TAGS[1], exception.getMessage());
         } catch (IOException exception) {
             Log.d(TAGS[1], exception.getMessage());
@@ -96,11 +119,25 @@ public class FoodSurveyHistoryManager {
         return loaded;
     }
 
-    public void loadTablesByMonth(Context activityContext, int monthIndex) {
+    public boolean loadTablesByMonth(Context activityContext, int monthIndex) {
+        createSaveFolder(activityContext);
+
         if (!tableHistory.isEmpty())
             tableHistory.clear();
-        String[] fileList = activityContext.fileList();
+        if (!tableDates.isEmpty())
+            tableDates.clear();
+        String[] fileList = saveFolder.list();
+        if (fileList != null && fileList.length > 1){
+            Arrays.sort(fileList, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareToIgnoreCase(o2);
+                }
+            });
+        }
+
         for (String fileName : fileList) {
+            Log.d(TAGS[1], "listing: " + fileName);
             String month = fileName.substring(fileName.length()-5, fileName.length()-3);
             int monthValue = 0;
             try{
@@ -113,12 +150,28 @@ public class FoodSurveyHistoryManager {
                 Log.d(TAGS[1], exception.getMessage());
             }
         }
+        return true;
     }
 
-    public void loadAllSavedTables(Context activityContext) {
+    public boolean loadAllSavedTables(Context activityContext) {
+        createSaveFolder(activityContext);
+
+        if( saveFolder == null ) return false;
         if (!tableHistory.isEmpty())
             tableHistory.clear();
-        String[] fileList = activityContext.fileList();
+        if (!tableDates.isEmpty())
+            tableDates.clear();
+        String[] fileList = activityContext.getDir(saveFolder.getName(), Context.MODE_PRIVATE).list();
+
+        if (fileList != null && fileList.length > 1){
+            Arrays.sort(fileList, new Comparator<String>() {
+                @Override
+                public int compare(String o1, String o2) {
+                    return o1.compareToIgnoreCase(o2);
+                }
+            });
+        }
+
         Pattern dateFormatPattern = Pattern.compile("\\d{2}-\\d{2}");
         for (String fileName : fileList) {
             if (fileName.matches(dateFormatPattern.pattern())) {
@@ -126,5 +179,6 @@ public class FoodSurveyHistoryManager {
                 tableHistory.add(loadTableByDate(activityContext, fileName));
             }
         }
+        return true;
     }
 }
