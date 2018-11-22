988f5ba1e50911caf274a397a6cdffff6345cc33
junweip <junwei_pan@sfu.ca>
2018-11-22 15:09:13 -0800

    The app can save image into sd card

--- app/src/main/java/ca/cmpt276/greengoblins/emission/AddMealActivity.java
+++ app/src/main/java/ca/cmpt276/greengoblins/emission/AddMealActivity.java
@@ -3,6 +3,7 @@ package ca.cmpt276.greengoblins.emission;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.ContentUris;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -40,10 +41,13 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import ca.cmpt276.greengoblins.foodsurveydata.Meal;
 import ca.cmpt276.greengoblins.fragments.Meal.MealListFragment;
+
+import static ca.cmpt276.greengoblins.emission.AddMealActivity.ImgUtils.saveImageToGallery;
 
 
 public class AddMealActivity extends AppCompatActivity {
@@ -204,7 +208,6 @@ public class AddMealActivity extends AppCompatActivity {
     private void take_photo() {
         String status= Environment.getExternalStorageState();
         if(status.equals(Environment.MEDIA_MOUNTED)) {
-            //创建File对象，用于存储拍照后的图片
             File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
             try {
                 if (outputImage.exists()) {
@@ -219,7 +222,6 @@ public class AddMealActivity extends AppCompatActivity {
             } else {
                 imageUri = Uri.fromFile(outputImage);
             }
-            //启动相机程序
             Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
             intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
             startActivityForResult(intent, TAKE_PHOTO);
@@ -303,6 +305,7 @@ public class AddMealActivity extends AppCompatActivity {
         if (imagePath != null) {
             Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
             mAddMealImageView.setImageBitmap(bitmap);
+            saveImageToGallery(getApplicationContext(),bitmap);
         }else {
             Toast.makeText(this,"failed to get image",Toast.LENGTH_SHORT).show();
         }
@@ -337,4 +340,33 @@ public class AddMealActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+    public static class ImgUtils {
+        public static boolean saveImageToGallery(Context context, Bitmap bmp) {
+            String storePath = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "eMissionImage";
+            File appDir = new File(storePath);
+            if (!appDir.exists()) {
+                appDir.mkdir();
+            }
+            String fileName = System.currentTimeMillis() + ".jpg";
+            File file = new File(appDir, fileName);
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                boolean isSuccess = bmp.compress(Bitmap.CompressFormat.JPEG, 60, fos);
+                fos.flush();
+                fos.close();
+                MediaStore.Images.Media.insertImage(context.getContentResolver(), file.getAbsolutePath(), fileName, null);
+                Uri uri = Uri.fromFile(file);
+                context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, uri));
+                if (isSuccess) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    }
+
 }
